3028d54fe921c86951c684547b6db0f717c9fa01
xiaoyang <xiaoyangxiaozhou@163.com>
2016-08-14 21:45:05 +0800

    add four

---
+++ 0814fuxi/four/01基础班第四天/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _01基础班第四天
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //求1到100之间所有整数的和
+            //int i = 1;
+            //int sum = 0;
+            //while(i<=100)
+            //{
+            //    sum += i;
+            //    i++;
+            //}
+            //Console.WriteLine("1到100的总和为{0}",sum);
+            //Console.ReadKey();
+            
+            //求1到100之间所有偶数的和
+            //int i = 1;
+            //int sum = 0;
+            //while (i <= 100)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        sum += i;
+            //    }
+            
+            //    i++;
+            //}
+            //Console.WriteLine("1到100的偶数总和为{0}", sum);
+            //Console.ReadKey();
+
+            //求1到100之间所有奇数的和
+            //int i = 1;
+            //int sum = 0;
+            //while (i <= 100)
+            //{
+            //    if (i % 2 == 1)
+            //    {
+            //        sum += i;
+            //    }
+
+            //    i++;
+            //}
+            //Console.WriteLine("1到100的奇数总和为{0}", sum);
+            //Console.ReadKey();
+
+            //int i = 1;
+            //int j = 1;
+            //while(i<=10)
+            //{
+            //    while (j <= 10)
+            //    {
+            //        Console.WriteLine("我是里面的{0}",j);
+            //        j++;
+            //    }
+            //    Console.WriteLine("我是外面的");
+            //    i++;
+            //}
+            //Console.ReadKey();
+
+            //要求用户输入用户名和密码，用户名只要不是admin，密码不是88888
+            //string userName = "";
+            //string userPwd = "";
+            //while (userName != "admin" || userPwd != "88888")
+            //{
+            //    Console.WriteLine("请输入用户名");
+            //    userName = Console.ReadLine();
+            //    Console.WriteLine("请输入密码");
+            //    userPwd = Console.ReadLine();
+            //    Console.WriteLine("输入错误，请重新输入");
+
+            //}
+            //Console.WriteLine("登录成功");
+            //Console.ReadKey();
+
+            //输入班级人数，然后依次输入学员成绩，计算班级学员的平均成绩和总成绩
+                     
+            //Console.WriteLine("请输入班级人数");
+            //int number = Convert.ToInt32(Console.ReadLine());
+            //int i=1;
+            //int score = 0;
+            //int sum = 0;
+            //while (i<=number)
+            //{
+            //    Console.WriteLine("请输入第{0}位学生的成绩",i);
+            //    score = Convert.ToInt32(Console.ReadLine());
+            //    sum += score;
+            //    i++;
+            //}
+            //Console.WriteLine("学员的总成绩为{0},平均成绩为{1}",sum,sum/number);
+            //Console.ReadKey();
+
+
+
+        }
+    }
+}
