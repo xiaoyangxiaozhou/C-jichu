f10124efbe9821105996eac5e4f52a32bb58c3d1
xiaoyang <xiaoyangxiaozhou@163.com>
2016-08-15 21:57:34 +0800

    add 05day

---
+++ 0814fuxi/five/05day/05dayfuxi/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _05dayfuxi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //不断要求用户输入一个数字(假如用户输入的数字都是正整数)，当用户输入end的时候显示刚才输入的数字中的最大值
+            //string input = "";
+            //int max = 0;
+            //while (input != "end")
+            //{
+            //    Console.WriteLine("请输入一个数字，当用户输入end时输出刚才的最大值");
+            //    input = Console.ReadLine();
+            //    if (input != "end")
+            //    {
+            //        try
+            //        {
+            //            int number = Convert.ToInt32(input);
+            //            if (number > max)
+            //            {
+            //                max = number;
+            //            }
+            //        }
+            //        catch
+            //        {
+            //            Console.WriteLine("输入错误，不是数字");
+            //        }
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("刚才数字中的最大值为{0}", max);
+            //    }
+
+            //}
+            //Console.ReadKey();
+
+        
+        }
+    }
+}
