9d80eaf6f7c2a0372c32c6ca2c90ba29cf7ad8fb
xiaoyang <xiaoyangxiaozhou@163.com>
2016-08-24 21:29:20 +0800

    add 07

---
+++ 0814fuxi/07/07day/01方法/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _01方法
+{
+    class Program
+    {
+        //字段 属于类的字段
+        //public static int _number = 10;
+        
+        
+        static void Main(string[] args)
+        {
+            //int a = 3;
+            //a=Test(a);
+            //Console.WriteLine(a);
+            ////Console.WriteLine(_number);
+            //Console.ReadKey();
+
+        }
+
+        //public static int Test(int a)
+        //{
+        //    a = a + 5;
+        //    return a;
+            
+        //}
+    }
+}
---
+++ 0814fuxi/07/07day/02方法练习/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _02方法练习
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //写一个方法，判断一个年份是否是闰年
+            //Console.WriteLine(" 请输入年份");
+            //int year = Convert.ToInt32(Console.ReadLine());
+            //bool b = IsRunNian(year);
+            //Console.WriteLine(b);
+            //Console.ReadKey();
+
+        }
+        /// <summary>
+        /// 判断给定的年份是否是闰年
+        /// </summary>
+        /// <param name="year">要判断的年份</param>
+        /// <returns>是否是闰年</returns>
+        //public static bool IsRunNian(int year)
+        //{
+            
+        //    bool b=(year % 400 == 0 )|| (year % 4 == 0 && year % 100 != 0))
+        //    return b;
+        
+        //}
+    }
+}
