a028b3b817a740b4770b2f8fd49d63994310b9bd
hongmf <306604496@qq.com>
2016-08-31 18:45:16 +0800

    gengxin

---
+++ 0814fuxi/10/10面象对象继承一/01命名空间/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace _01命名空间
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //List<int> list = new List<int>();
+            //File
+        }
+    }
+}
---
+++ 0814fuxi/10/10面象对象继承一/02字符串的方法一/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace _02字符串的方法一
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string s = "abcdefg";
+            ////s[0] = 'b';  //不能这样做，因为是只读的
+            ////首先将字符串转换为char类型数组
+            //char[] chs = s.ToCharArray();
+            //chs[0]='b';
+            ////将字符数组转为字符串
+            //s = new string(chs);
+            ////可以将string看做char类型的只读数组
+            //Console.WriteLine(s);
+            //Console.ReadKey();
+
+            //StringBuilder sb = new StringBuilder();
+            //string str = null;
+
+            ////创建计时器
+            //Stopwatch sw=new Stopwatch();
+            //sw.Start();
+            //for (int i = 0; i < 10000; i++)
+            //{
+            //    str += i;
+            //    sb.Append(i);
+            //}
+            //sw.Stop();
+            //Console.WriteLine(sb.ToString());
+            //Console.WriteLine(sw.Elapsed);
+            //Console.ReadKey();
+
+
+        }
+    }
+}
